fa16e682776228768959549de4b35613fdf5e318
Aditya Kumar Jha <jha52172@gmail.com>
2026-04-07 02:12:09 +0530

    Fixed missing app icon bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ dependencies {
 
     // Cryptography (Libsodium Wrapper)
     // https://github.com/terl/lazysodium-android
-    implementation("com.goterl:lazysodium-android:5.1.0@aar")
-    implementation("net.java.dev.jna:jna:5.13.0@aar")
+    implementation("com.goterl:lazysodium-android:5.1.0")
+    implementation("net.java.dev.jna:jna:5.13.0")
 
     // Secure Local Storage (SQLCipher)
     implementation("net.zetetic:android-database-sqlcipher:4.5.4")
